1cd0cfb902b2146115c9505e652ac353e2438225
GiangDT <giangdt1215@gmail.com>
2023-01-07 23:48:19 +0700

    First Odin js exercises

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,11 @@
-const reverseString = function() {
+const reverseString = function(value) {
+    let reservedStr = '';
+    for (let index = value.length; index >= 0; index--) {
+        reservedStr += value.charAt(index);
+    }
+    return reservedStr;
 
+    //return value.split('').reverse().join('');
 };
 
 // Do not edit below this line
--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,21 @@
-const sumAll = function() {
+const sumAll = function(a, b) {
+    if(!Number.isInteger(a) || !Number.isInteger(b))
+        return 'ERROR';
+    if(a < 0 || b < 0)
+        return 'ERROR';
 
+    if(b < a){
+        let tmp = a;
+        a = b;
+        b = tmp;
+    }
+
+    let sum = 0;
+    for (let index = a; index <= b; index++) {
+        sum += index;
+    }
+
+    return sum;
 };
 
 // Do not edit below this line
